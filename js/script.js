ab64558845518890435f3bcfbf1f0181a13cb8f0
Encohify <encohify@Encohifys-MacBook-Pro-4.local>
2019-11-23 14:05:51 +0800

    fixed the header color changing issue

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-hdjka
+"use strict";
+
+const htmlTag = document.querySelector('html');
+const bodyTag = document.querySelector('body');
+const myNav = document.querySelector('header');
+
+let scrolled = () => {
+  let dec = scrollY/(bodyTag.scrollHeight - innerHeight);
+  return Math.floor(dec*100);
+}
+addEventListener('scroll',()=>{
+  myNav.style.setProperty('background',scrolled() > 40 ?
+  "var(--color2)": "var(--color1)");
+})
