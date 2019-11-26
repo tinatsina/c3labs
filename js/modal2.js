8544ae52eafc1ab516a14c364acbd44d4700564c
Encohify <encohify@Encohifys-MacBook-Pro-4.local>
2019-11-26 23:05:06 +0800

    adding pop-ups

---
+++ js/modal2.js
@@ -0,0 +1,12 @@
+// Get the modal
+var modal2 = document.getElementById("myModal2");
+
+// Get the button that opens the modal
+var btn2 = document.getElementById("countries2");
+
+
+
+// When the user clicks on the button, open the modal
+btn2.onclick = function() {
+  modal2.style.display = "block";
+}
