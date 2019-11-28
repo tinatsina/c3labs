7972d3fea5e2ac61c85475e6e7a454989d7bd8d8
Encohify <encohify@Encohifys-MacBook-Pro-4.local>
2019-11-28 15:32:36 +0800

    Fixed modal issue

--- js/modal1.js
+++ js/modal1.js
@@ -1,12 +1,14 @@
 // Get the modal
-var modal = document.getElementById("myModal1");
+var modal = document.getElementById('id01');
+var modal2 = document.getElementById('id02');
+var modal3 = document.getElementById('id03');
+var modal4 = document.getElementById('id04');
+var modal5 = document.getElementById('id05');
+var modal6 = document.getElementById('id06');
 
-// Get the button that opens the modal
-var btn = document.getElementById("countries1");
-
-
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function closeOnClick(event) {
+  if (event.target.className === "w3-modal") {
+    event.target.style.display = "none";
+  }
 }
